79ba5510e434439d660e1056c0606be530d4bd3b
Patta2475 <6930613013@psu.ac.th>
2026-09-16 15:17:35 +0700

    Delete lab1 directory

--- lab1/User.java
+++
@@ -1,6 +0,0 @@
-public class User {
-    public static void main(String[] args){
-        TV tv1 = new TV("TV");
-        System.out.println("TV1: channel = " + tv1.channel + ", brightness = " + tv1.brightness + ", gamma = " + tv1.gamma + ", name = " + tv1.name);
-    }
-}
